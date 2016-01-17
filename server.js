d8df7c968efada1e59fedf4e92a8045cd0439912
longchiwen <bostjan@studiopesec.com>
2016-01-17 01:21:51 +0100

    Added dotenv package. Postinstall script will initialize .env file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 var express = require("express");
 var process = require("process");
 var path = require("path");
 var app =  express();
-var port = process.env.PORT || 3000;
+const PORT = process.env.PORT||3000;
+const HOST = process.env.HOST||"localhost";
 
 app.use("/assets/js",express.static(path.resolve(__dirname,'./dist/assets/js')));
 app.use("/assets/css",express.static(path.resolve(__dirname,'./dist/assets/css')));
@@ -11,7 +13,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname,'./src/public/index.html'));
 });
 
-app.listen( process.env.PORT || 3000, function () {
+app.listen(PORT,HOST, function () {
   /* eslint-disable no-console */
-  console.log('Express server started on port %s', port);
+  console.log('Express server started on %s:%s',HOST, PORT);
 })
---
+++ tools/postinstall.js
@@ -0,0 +1,14 @@
+var path = require("path");
+var fs = require("fs");
+var file = path.join(path.resolve(__dirname,"../"),".env");
+
+fs.stat(file, function(err, stat) {
+    if(err == null) {
+        console.log('.env file already exists. Skipping ...');
+    } else if(err.code == 'ENOENT') {
+        fs.writeFile(file, '# configuration file');
+        console.log('.env file created');
+    } else {
+        console.log('Error while accessing .nev file: '+ err.code);
+    }
+});
